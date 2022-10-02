bd2920fb5a37b37f7b807d4ab198dedecff5c2c6
duanenielsen <duane.nielsen.rocks@gmail.com>
2022-10-02 15:01:43 -0700

    Added augmentations.

--- README.md
+++ README.md
@@ -7,3 +7,23 @@ Ubuntu 20.4
 ```
 sudo apt-get install python3-tk
 ```
+
+Create virtual environment
+
+```
+pip -m venv ./venv
+./venv/bin/activate
+```
+
+
+Install pytorch for your CUDA version, eg CUDA 11.6
+
+```commandline
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+```
+
+Install requirements
+
+```commandline
+pip install -r requirements.txt
+```
--- train.py
+++ train.py
@@ -3,14 +3,21 @@ import flash
 from flash.image import ImageClassificationData, ImageClassifier
 import pytorch_lightning as pl
 import wandb
-from wandb.plot import confusion_matrix
-from torchvision.transforms.functional import to_tensor
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.plugins import DDPPlugin
 import argparse
-import random
 from torchmetrics import ConfusionMatrix
+from flash.image.classification.input_transform import AlbumentationsAdapter
+import numpy as np
+import albumentations as alb
+from flash import InputTransform
+from torchvision import transforms as T
+from flash.core.data.transforms import ApplyToKeys
+from flash.core.data.io.input import DataKeys
+from typing import Tuple, Union
+from dataclasses import dataclass
+from torchvision.transforms.autoaugment import AutoAugmentPolicy
 
 
 def load_labels(datamodule):
@@ -83,17 +90,98 @@ if __name__ == '__main__':
                                    save_top_k=1, mode='max',
                                    monitor="val_accuracy")
 
+    def mixup(batch, alpha=1.0):
+        images = np.array(batch["input"])
+        targets = batch["target"].float().unsqueeze(1)
+
+        lam = np.random.beta(alpha, alpha)
+        perm = torch.randperm(images.size(0))
+
+        batch["input"] = images * lam + images[perm] * (1 - lam)
+        batch["target"] = targets * lam + targets[perm] * (1 - lam)
+        return batch
+
+    class MixUpInputTransform(InputTransform):
+        def __init__(self, image_size, mean, std):
+            self.image_size = image_size
+            self.mean, self.std = mean, std
+            super().__init__()
+
+        def train_per_sample_transform(self):
+            return T.Compose(
+                [
+                    ApplyToKeys(
+                        DataKeys.INPUT,
+                        T.Compose(
+                            [
+                                T.ToTensor(),
+                                T.Resize(self.image_size),
+                                T.Normalize(self.mean, self.std),
+                                T.RandomHorizontalFlip(),
+                            ]
+                        ),
+                    ),
+                    ApplyToKeys(DataKeys.TARGET, torch.as_tensor),
+                ]
+            )
+
+        # def train_input_per_sample_transform(self):
+        #     return AlbumentationsAdapter(alb.HorizontalFlip(p=0.5))
+
+        # This will be applied after transferring the batch to the device!
+        def train_per_batch_transform_on_device(self):
+            return mixup
+
+
+    @dataclass
+    class ImageClassificationInputTransform(InputTransform):
+
+        image_size: Tuple[int, int] = (196, 196)
+        mean: Union[float, Tuple[float, float, float]] = (0.485, 0.456, 0.406)
+        std: Union[float, Tuple[float, float, float]] = (0.229, 0.224, 0.225)
+
+        def per_sample_transform(self):
+            return T.Compose(
+                [
+                    ApplyToKeys(
+                        DataKeys.INPUT,
+                        T.Compose([T.ToTensor(), T.Resize(self.image_size), T.Normalize(self.mean, self.std)]),
+                    ),
+                    ApplyToKeys(DataKeys.TARGET, torch.as_tensor),
+                ]
+            )
+
+        def train_per_sample_transform(self):
+            return T.Compose(
+                [
+                    ApplyToKeys(
+                        DataKeys.INPUT,
+                        T.Compose(
+                            [
+                                T.AutoAugment(AutoAugmentPolicy.CIFAR10),
+                                T.ToTensor(),
+                                T.Resize(self.image_size),
+                                T.Normalize(self.mean, self.std),
+                                T.RandomHorizontalFlip(),
+                            ]
+                        ),
+                    ),
+                    ApplyToKeys(DataKeys.TARGET, torch.as_tensor),
+                ]
+            )
+
     datamodule = ImageClassificationData.from_folders(
         train_folder=f"{data_root_dir}/traffic_cones/train/",
         val_folder=f"{data_root_dir}/traffic_cones/val/",
         batch_size=8,
-        transform_kwargs={"image_size": (196, 196), "mean": (0.485, 0.456, 0.406), "std": (0.229, 0.224, 0.225)},
+        transform=ImageClassificationInputTransform,
+        transform_kwargs={"image_size": (512, 512), "mean": (0.485, 0.456, 0.406), "std": (0.229, 0.224, 0.225)},
     )
 
     # Init or load model
     if args.resume is None:
         model = ImageClassifier(backbone="resnet18", labels=datamodule.labels,
-                                #lr_scheduler=("MultiStepLR", {"milestones": [10, 50], "gamma": 0.1}),
+                                # lr_scheduler=("MultiStepLR", {"milestones": [10, 50], "gamma": 0.1}),
                                 learning_rate=1e-3
                                 )
     else:
@@ -104,7 +192,8 @@ if __name__ == '__main__':
                             strategy=DDPPlugin(find_unused_parameters=False),
                             logger=wandb_logger,
                             enable_checkpointing=True,
-                            callbacks=[LearningRateMonitor(), checkpoint_f, wandb_image_f, WandbConfusionMatrixCallback()])
+                            callbacks=[LearningRateMonitor(), checkpoint_f, wandb_image_f,
+                                       WandbConfusionMatrixCallback()])
     trainer.finetune(model, datamodule=datamodule, strategy="freeze")
 
     with open('labels.txt', 'w') as f:
